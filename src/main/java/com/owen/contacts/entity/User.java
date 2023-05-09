1cb87d177af378e298c26bd2f44523253e8bbd39
owantdelhiver <owenboutet@gmail.com>
2023-05-09 10:20:11 +0200

    join with contact

--- src/main/java/com/owen/contacts/entity/User.java
+++ src/main/java/com/owen/contacts/entity/User.java
@@ -1,10 +1,17 @@
 package com.owen.contacts.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class User extends UserAbstract{
     private String mot_de_passe;
+    @OneToMany
+    @JoinColumn(name = "id_user")
+    private List<Contact> contacts;
 
     public User() {
     }
